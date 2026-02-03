27bedd8d85a1a1fd734b7076e5c3240316fa0483
KhimPheaktra <khimpheaktra04@gmail.com>
2026-02-03 21:29:29 +0700

    fix: correct casing for PageHeader.css

--- src/components/layout/page_header/pageHeader.tsx
+++ src/components/layout/page_header/pageHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col, Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import './PageHeader.css';
+import './pageHeader.css';
 
 interface PageHeaderProps {
     title: string;
